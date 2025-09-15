60f89fbc3b79a6de3f02bf67da1c32d7f9dc768a
diyorarti <diyorarti@gmail.com>
2025-09-15 06:50:20 +0500

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
-
-const Home = () => {
+const page = () => {
   return (
-    <div className="text-3xl text-center p-10">Welcome to NEXT-JS</div>
-  )
-}
+    <div className="">
+      <h1 className="">TailwindCSS is fun</h1>
+      <h1 className="h1-bold font-space-grotesk">TailwindCSS is fun</h1>
+    </div>
+  );
+};
 
-export default Home
+export default page;
